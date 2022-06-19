133548f034626d9af49f9d2c58141bc5ead5f6b0
Damián López <lopezrunco@gmail.com>
2022-06-19 18:15:31 +0530

    Navbar basic component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,29 @@
+import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <Link href="/">BATALLA DE SARANDI</Link>
+      <ul>
+        <li className={styles.listItem}>
+          <Link href="/">Inicio</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/nosotros">Nosotros</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/galeria">Galería</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/novedades">Novedades</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/contacto">Contacto</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
