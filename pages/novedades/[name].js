a5a876a9ee2a3bb759b51ea55ab81e403facc2f7
Damián López <lopezrunco@gmail.com>
2022-06-27 11:16:57 +0530

    NewItem video modal

--- pages/novedades/[name].js
+++ pages/novedades/[name].js
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import { useState } from 'react'
 import { data } from '../../data'
 import PageHeader from '../../components/PageHeader'
 import styles from '../../styles/New.module.css'
 
 const New = ({ newItem }) => {
+    const [openVideoModal, setOpenVideoModal] = useState(false)
+    const [videoSrc, setVideoSrc] = useState('')
+    const handleClick = () => {
+        setOpenVideoModal(!openVideoModal)
+    }
+
     return (
         <>
             <PageHeader
@@ -34,7 +40,7 @@ const New = ({ newItem }) => {
                             <p>{newItem.desc}</p>
                             <p>{newItem.longDesc}</p>
                         </div>
-                        <div className={styles.videoThumb} onclick="toggleModal()">
+                        <div className={styles.videoThumb} onClick={handleClick}>
                             <img src={`/images/${newItem.videoThumb}`} alt={newItem.title} />
                             <div className={styles.playIcon}>
                                 <a>
@@ -44,12 +50,14 @@ const New = ({ newItem }) => {
                         </div>
                     </div>
 
-                    <div id="video-modal" className={styles.videoModal}>
-                        <div className={styles.closeIcon} id="modal-close-icon" onClick="toggleModal()">
-                            <i className="fas fa-times"></i>
+                    {openVideoModal && (
+                        <div className={styles.videoModal}>
+                            <div className={styles.closeIcon} onClick={handleClick}>
+                                <i className="fas fa-times"></i>
+                            </div>
+                            <iframe src={`https://www.youtube.com/embed/${newItem.videoSrc}`} title="YouTube video player" frameBorder="0" allow="autoplay" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                         </div>
-                        <iframe src={`https://www.youtube.com/embed/${newItem.videoSrc}`} title="YouTube video player" frameBorder="0" allow="autoplay" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    </div>
+                    )}
 
                 </article>
             </section>
